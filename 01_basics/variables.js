c3c02c2e257af69954ca29ec166f6b3a97a30679
ManishSonawane24 <manishsonawane2424@gmail.com>
2025-01-01 12:08:49 +0530

    made some changes

---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+let name = "manish";
+var age = 25;
+let isStudent = true;   
+const marks = 75.5;
+let subjects = ["Maths", "Science", "English"];
+let student = {
+    name: "manish", 
+    age: 25,
+    isStudent: true,
+    marks: 75.5,
+    subjects: ["Maths", "Science", "English"]   
+};  
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(marks); 
+console.log(subjects);  
+console.log(student);   
+
