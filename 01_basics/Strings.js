a9c4d402b299e1ff44923c83e6f10455038fbcb9
ManishSonawane24 <manishsonawane2424@gmail.com>
2025-01-01 15:39:04 +0530

    Strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,17 @@
+let name = "manish";
+var age = 25;
+let isStudent = true;   
+const marks = 75.5;
+let subjects = ["Maths", "Science", "English"];
+
+
+console.log(`Hello my name is ${name} and I am ${age} years old and I have scored ${marks} marks in my exams.`);
+
+const gameName = new String("pubg");
+
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -3,18 +3,20 @@ var age = 25;
 let isStudent = true;   
 const marks = 75.5;
 let subjects = ["Maths", "Science", "English"];
-let student = {
-    name: "manish", 
-    age: 25,
-    isStudent: true,
-    marks: 75.5,
-    subjects: ["Maths", "Science", "English"]   
-};  
+// let student = {
+//     name: "manish", 
+//     age: 25,
+//     isStudent: true,
+//     marks: 75.5,
+//     subjects: ["Maths", "Science", "English"]   
+// };  
+
+console.log(`Hello my name is ${name} and I am ${age} years old and I have scored ${marks} marks in my exams.`);
 
 console.log(name);
 console.log(age);
 console.log(isStudent);
 console.log(marks); 
 console.log(subjects);  
-console.log(student);   
+// console.log(student);   
 
