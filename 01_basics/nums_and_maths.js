a3b3e61daff890de5161111797dcd4fb894010d9
ManishSonawane24 <manishsonawane2424@gmail.com>
2025-01-02 09:51:30 +0530

    Maths part is added

--- 01_basics/nums_and_maths.js
+++ 01_basics/nums_and_maths.js
@@ -23,5 +23,10 @@ console.log(Math.random());
 console.log(Math.PI);
 
 
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
 
+const min = 10;
+const max = 20;
 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
