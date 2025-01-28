e485ac7cd2230c4e0ce7feb63b32a09e0e181af4
ManishSonawane24 <manishsonawane2424@gmail.com>
2025-01-28 19:00:17 +0530

    types of functions

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,38 @@
+// var c = 300;
+
+// let a = 100;
+// let b = 200;
+// if(true) {
+//     let a = 10;
+//     var d = 400;
+//     c = 30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(d);
+// }
+
+// console.log(a);
+
+function one() {
+    username = "manish"
+
+    function two() {
+        const password = "password"
+        console.log(username)
+    }
+    // console.log(password);
+    two()
+}
+    
+// one()
+
+function addOne(num){
+    return num + 1
+}
+addOne(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
